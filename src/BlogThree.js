e7b43d21726fa55fba04892fcd3d8e7434d44404
GulbeEvija <evija.gulbe@outlook.com>
2024-02-01 20:01:46 -0500

    updating all other buttons in the blog number 3

--- src/BlogThree.js
+++ src/BlogThree.js
@@ -29,37 +29,49 @@ function BlogThree() {
             <div className="container">
                 <h3 className="blogHeader">Summer Sneakers</h3>
                 <img src={summerTwo} width="420px" height="320px" alt="Shoes"/>
-                <button className='shoeButton'><a href={`/shop/White-leather-sneakers`}  rel="noreferrer" className='shopLink'>Buy this shoe!</a></button>
+                <Link to={`/shop/White-leather-sneakers`}>
+                    <button className='shoeButton shopLink'>Buy this shoe!</button>
+                </Link>
             </div>
 
             <div className="container">
                 <h3 className="blogHeader">Espadrilles</h3>
                 <img src={summerThree} width="420px" height="320px" alt="Shoes"/>
-                <button className='shoeButton'><a href={`/shop/Red-Espadrilles`}  rel="noreferrer" className='shopLink'>Buy this shoe!</a></button>
+                <Link to={`/shop/Red-Espadrilles`}>
+                    <button className='shoeButton shopLink'>Buy this shoe!</button>
+                </Link>
             </div>
 
             <div className="container">
                 <h3 className="blogHeader">Ballet Flats</h3>
                 <img src={summerFour} width="420px" height="320px" alt="Shoes"/>
-                <button className='shoeButton'><a href={`/shop/Pink-leather-flats`}  rel="noreferrer" className='shopLink'>Buy this shoe!</a></button>
+                <Link to={`/shop/Pink-leather-flats`}>
+                    <button className='shoeButton shopLink'>Buy this shoe!</button>
+                </Link>
             </div>
 
             <div className="container">
                 <h3 className="blogHeader">Summer Loafers</h3>
                 <img src={summerFive} width="420px" height="320px" alt="Shoes"/>
-                <button className='shoeButton'><a href={`/shop/Summer-suede-loafers`}  rel="noreferrer" className='shopLink'>Buy this shoe!</a></button>
+                <Link to={`/shop/Summer-suede-loafers`}>
+                    <button className='shoeButton shopLink'>Buy this shoe!</button>
+                </Link>
             </div>
 
             <div className="container">
                 <h3 className="blogHeader">Slide Sandals</h3>
                 <img src={summerSix} width="420px" height="320px" alt="Shoes"/>
-                <button className='shoeButton'><a href={`/shop/Leather-slides`}  rel="noreferrer" className='shopLink'>Buy this shoe!</a></button>
+                <Link to={`/shop/Leather-slides`}>
+                    <button className='shoeButton shopLink'>Buy this shoe!</button>
+                </Link>
             </div>
 
             <div className="container">
                 <h3 className="blogHeader">Fisherman Sandals</h3>
                 <img src={summerSeven} width="420px" height="320px" alt="Shoes"/>
-                <button className='shoeButton'><a href={`/shop/Fishermen-sandals`}  rel="noreferrer" className='shopLink'>Buy this shoe!</a></button>
+                <Link to={`/shop/Fishermen-sandals`}>
+                    <button className='shoeButton shopLink'>Buy this shoe!</button>
+                </Link>
             </div>
             </div>
             
